dec1796e4b52e908b43a633b4fef489f79cc4791
John Armbruster <jaboyak@gmail.com>
2017-12-09 22:43:03 -0700

    updated flow and updated dashboard styles

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -19,7 +19,8 @@ class Dashboard extends Component<Props, null> {
       height: 300,
       backgroundColor: 'beige',
       border: '1px solid #c9c5c2',
-      boxShadow: "3px 3px 47px 0px rgba(0,0,0,0.5)"
+      boxShadow: "3px 3px 47px 0px rgba(0,0,0,0.5)",
+      marginRight: 15
     }
 
     let faStyles = {
@@ -34,16 +35,16 @@ class Dashboard extends Component<Props, null> {
         <Row style={{marginTop: '15vh'}}>
           <Col
             style={colStyles}
-            onClick={() => this.props.history.push('/account-info')}
+            onClick={() => this.props.history.push('/appointments')}
           >
             <div className="text-center">View Past Appointments</div>
             <FontAwesome className="text-center" name='book' style={faStyles} />
           </Col>
           <Col
             style={colStyles}
-            onClick={() => this.props.history.push('/appointments')}
+            onClick={() => this.props.history.push('/schedule')}
           >
-            <div className="text-center">See your doctor or schedule an appointment</div>
+            <div className="text-center">Schedule a new appointment</div>
             <FontAwesome className="text-center" name='calendar' style={faStyles} />
           </Col>
           <Col
